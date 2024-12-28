9671680a4704ded6d015929915923ce3b2e38c06
Fabrizio <65259076+Fabrizz@users.noreply.github.com>
2024-12-28 04:00:19 -0300

    Add project files.

---
+++ ZPLwasm/Program.cs
@@ -0,0 +1,32 @@
+﻿using BinaryKits.Zpl.Viewer;
+using System.Runtime.InteropServices.JavaScript;
+
+return;
+
+public partial class Sample
+{
+    // Accessible from JS
+    [JSExport]
+    internal static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public partial class Lib
+{
+    // Accessible from JS
+    [JSExport]
+    internal static byte[] CreateLabel(String a, double h, double w, int pd)
+    {
+        IPrinterStorage printerStorage = new PrinterStorage();
+        var drawer = new ZplElementDrawer(printerStorage);
+
+        var analyzer = new ZplAnalyzer(printerStorage);
+        var analyzeInfo = analyzer.Analyze(a);
+
+        var imageData = drawer.Draw(analyzeInfo.LabelInfos[0].ZplElements, h, w, pd); // l, 300, 300, 8
+
+        return imageData;
+    }
+}
---
+++ ZPLwasm/main.js
@@ -0,0 +1,16 @@
+﻿// Set up the .NET WebAssembly runtime
+import { dotnet } from './dotnet.js'
+
+// Get exported methods from the .NET assembly
+const { getAssemblyExports, getConfig } = await dotnet
+    .withDiagnosticTracing(false)
+    .create();
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+// Access JSExport methods using exports.<Namespace>.<Type>.<Method>
+const result = exports.Lib.CreateLabel("^XA^FT100,100^A0N,67,0^FDTestLabel^FS^XZ", 300, 300, 8);
+
+// Display the result of the .NET method
+document.getElementById("out").innerHTML = `Result: ${result}`;
